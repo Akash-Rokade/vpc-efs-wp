cc0bcf232fe3d33817bb6eb0192289f674379f5f
Akash Rokade <akashdrokade@gmail.com>
2022-04-21 15:40:08 +0530

    Update install-wp.sh

--- install-wp.sh
+++ install-wp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+efs_id=${efs_id}
+
+apt update
 apt install apache2 php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc unzip nfs-common -y
 cd /var/www/html/
 rm -f index.html
 cd ..
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0e264e010188f51df.efs.us-east-1.amazonaws.com:/ /var/www/html/
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $efs_id.efs.us-east-1.amazonaws.com:/ /var/www/html/
 df -h
 a2enmod rewrite
 cd html
